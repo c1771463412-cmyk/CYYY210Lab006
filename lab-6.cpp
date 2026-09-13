74a2854784950ca22ded39e02ecbc1082ec16141
Yuyi Chen <c1771463412@gmail.com>
2026-09-12 19:11:43 -0700

    Finish enterArrayData

--- lab-6.cpp
+++ lab-6.cpp
@@ -3,3 +3,32 @@
 #include <iostream>
 
 using namespace std;
+
+// Size of the array
+const int SIZE = 5;
+
+// Prototypes
+void enterArrayData(double *);
+void outputArrayData(double *);
+double sumArray(double *);
+
+int main() {
+    double *arr = nullptr;
+    arr = new double[SIZE];
+
+    enterArrayData(arr);
+
+    return 0;
+}
+
+// Function to enter data into the array
+void enterArrayData(double *arr) {
+    cout << "Data entry for the array: " << endl;
+
+    for (int i = 0; i < SIZE; i++) {
+        cout << "\t> Element #" << i << ": ";
+        cin >> *(arr + i);
+    }
+
+    cout << "Data entry complete." << endl;
+}
